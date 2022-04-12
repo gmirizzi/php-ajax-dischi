257cb2a5d94b144d756ad769e8bd91104d6a2ba0
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-12 15:26:51 +0200

    stile

--- index.php
+++ index.php
@@ -1,28 +1,38 @@
 <?php include 'data.php' ?>
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+
+    <!-- link css -->
+    <link rel="stylesheet" href="stile.css">
     <title>PHP Dischi</title>
 </head>
+
 <body>
     <main>
         <div id="container">
-            <?php 
+            <?php
             foreach ($arrDischi as $disco) { ?>
                 <div class="card">
-                    <img src="<?= $disco['poster']?>" alt="<?= $disco['titolo']?>">
+                    <img src="<?= $disco['poster'] ?>" alt="<?= $disco['titolo'] ?>">
                     <div>
-                        <h2><?= $disco['titolo']?></h2>
-                        <div class="artista"><?= $disco['artista']?></div>
-                        <div class="anno"><?= $disco['anno']?></div>
+                        <h2><?= $disco['titolo'] ?></h2>
+                        <div class="artista"><?= $disco['artista'] ?></div>
+                        <div class="anno"><?= $disco['anno'] ?></div>
                     </div>
                 </div><?php
-            }
-            ?>
+                    }
+                        ?>
         </div>
     </main>
 </body>
+
 </html>
