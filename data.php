8cb364fbe1c645c41a626a5f75cc927febfeaf42
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-12 14:02:32 +0200

    first commit

---
+++ data.php
@@ -0,0 +1,10 @@
+<?php
+$arrDischi = [
+    [
+        'poster' => 'https://images.genius.com/c3b54f2b34473bd3d41448f8ea58811e.1000x1000x1.jpg',
+        'titolo' => 'Persona',
+        'artista' => 'Marrcash',
+        'anno' => '2019',
+    ],
+]
+?>
